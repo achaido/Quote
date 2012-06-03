c3cf5ca7665e861d6e8ef4ccef7793aac7416ebf
Petter Palander <petter@palander.se>
2012-06-04 00:17:45 +0200

    changed to quotation marks

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
+
 window.onload = function() {
   
-  
+
 
   // get quote container
   var quote = document.getElementById("slider");
@@ -22,19 +23,15 @@ window.onload = function() {
   // with the data, you get from the JSON, do something
   myJsonpCallback = function(data)
   {
-    
-      
 
       // get all posts
        var posts = data.response.posts
 
-       // get a random post
-       var post = data.response.posts[Math.floor(Math.random() * posts.length)]
-
-
-      
       injectJson = function(){
         for (i = 0; i < posts.length; i++){
+          
+          // get a random post (generate here to make it random)
+          var post = data.response.posts[Math.floor(Math.random() * posts.length)]
           
           if(i === 1){
             $('#slider ul li').css("display", "block");   
@@ -42,39 +39,26 @@ window.onload = function() {
              $('#slider ul li').css("display", "none"); 
           }
 
-          $('#slider ul').append('<li><div><h1>"' + posts[i].text + '"</h1>' + "<h5>-" + posts[i].source + "</h5></div></li>");
-          $("body").animate({ opacity: "1"}, 1000);
+          $('#slider ul').append('<li><div><h1>&#8220;' + post.text + '&#8221;</h1>' + '<h5>-' + post.source + '</h5></div></li>');
+          
           
 
         }    
         // When done the loop do this:
         new Swipe(document.getElementById('slider'));
+        $("body").animate({ opacity: "1"}, 1000);
         $("#slider ul li h1").fitText(1.2);
         $("#slider ul li h5").fitText(3.4);
         $("#slider ul li h1").css("line-height", "1em");
         
       }
       injectJson(); 
-      
 
 
-       
       changeTitle = function(post){
         document.title = post.text;
       }
       changeTitle(post);
-    
-      
-      // console.log(data.response.posts[0].type);
-      // console.log(post.type)
-     
-      // If post is of type 'quote' then do something
-      /*
-      if(post.type === "quote"){
-       // injectJson();      
-      }
-      */
-      
       
       // Keyboard shortcuts
       $('body').bind('keydown', 'space', function(e){
@@ -88,13 +72,13 @@ window.onload = function() {
       $('body').bind('keydown', 'left', function(e){
         e.preventDefault();
             console.log('left');
-           // $("#quote-container").animate({ left: "100%"}, 300);
+            $("#slide").animate({ left: "100%"}, 300);
         });
       
       $('body').bind('keydown', 'right', function(e){
         e.preventDefault();
             console.log('right');
-          //  $("#quote-container").animate({ right: "100%"}, 300);
+            $("#slide").animate({ right: "100%"}, 300);
         });
       
   }
